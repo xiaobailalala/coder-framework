169f40c0232a91425f760ad91c820ad42faf000b
xiaobailalala <1051973784@qq.com>
2019-09-05 12:44:16 +0800

    init project

---
+++ validate/src/main/java/com/coder/framework/validate/annotation/Verify.java
@@ -0,0 +1,12 @@
+package com.coder.framework.validate.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Verify {
+
+
+
+}
---
+++ validate/src/main/java/com/coder/framework/validate/aspect/VerifyMethodArgumentResolver.java
@@ -0,0 +1,49 @@
+package com.coder.framework.validate.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+import java.util.Arrays;
+
+/**
+ * Copyright © 2018 eSunny Info. Developer Stu. All rights reserved.
+ * <p>
+ * code is far away from bug with the animal protecting
+ * <p>
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ * @author zpx
+ * Build File @date: 2019/9/5 0:05
+ * @version 1.0
+ * @description TODO
+ */
+@Aspect
+public class VerifyMethodArgumentResolver {
+
+    @Pointcut("@annotation(com.coder.framework.validate.annotation.Verify)")
+    public void doAspect() {}
+
+    @Before("doAspect()")
+    public void doBefore(JoinPoint joinPoint) {
+        System.out.println(Arrays.toString(joinPoint.getArgs()));
+    }
+
+}
